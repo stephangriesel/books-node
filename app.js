6a56fe2b4f20fd185a6cb13fa4a28f4897de2bea
Stephan Griesel <sgriesel@gmail.com>
2019-02-22 06:22:04 +0100

    set up template views

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
+const hbs = require('hbs');
 
 //Routes
 app.get("/",function(req, res) {
-    res.send("Hello test");
+    res.render("index",);
 });
 
 app.listen("3000", function() {
     console.log("Connected on port 3000")
 });
+
+// Handlebars 
+app.set('view engine', 'hbs');
+app.set("views", __dirname +  "/views");
+hbs.registerPartials(__dirname + "/views/partials");
