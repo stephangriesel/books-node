eee6f4ee1f0f533b2ca1b61e3d778db25a4fbbe8
Stephan Griesel <sgriesel@gmail.com>
2019-02-22 06:36:13 +0100

    add article view

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ const express = require("express");
 const app = express();
 const hbs = require('hbs');
 
+// Handlebars 
+app.set('view engine', 'hbs');
+app.set("views", __dirname +  "/views");
+hbs.registerPartials(__dirname + "/views/partials");
+
 //Routes
 app.get("/",function(req, res) {
     res.render("index",);
+});
+
+app.get("/articles/add",function(req, res) { 
+    res.render("add_article",);
 });
 
 app.listen("3000", function() {
     console.log("Connected on port 3000")
 });
 
-// Handlebars 
-app.set('view engine', 'hbs');
-app.set("views", __dirname +  "/views");
-hbs.registerPartials(__dirname + "/views/partials");
+
