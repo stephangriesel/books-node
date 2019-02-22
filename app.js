c52f57065162ac76db308e06a49b95321c8a9124
Stephan Griesel <sgriesel@gmail.com>
2019-02-22 06:46:40 +0100

    add array & update index view

--- app.js
+++ app.js
@@ -9,7 +9,30 @@ hbs.registerPartials(__dirname + "/views/partials");
 
 //Routes
 app.get("/",function(req, res) {
-    res.render("index",);
+    let articles = [
+        {
+            id:1,
+            title: "Nommer 1",
+            author: "Stephan Griesel",
+            body: "Description of article 1"
+        },
+        {
+            id:2,
+            title: "Nommer 2",
+            author: "Stephan Griesel",
+            body: "Description of article 2"
+        },
+        {
+            id:3,
+            title: "Nommer 3",
+            author: "Stephan Griesel",
+            body: "Description of article 3"
+        },
+    ]
+    res.render("index", {
+        title: 'Articles',
+        articles: articles
+    });
 });
 
 app.get("/articles/add",function(req, res) { 
